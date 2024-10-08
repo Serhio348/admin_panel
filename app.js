a290c4e1d78abb1619e11ec6ef3619ffd7b21684
Siarhei Sidarovich <serhiosidorovich@gmail.com>
2024-10-08 16:44:13 +0300

    feat:add api function users

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-import express, { json, urlencoded } from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+const  express = require('express');
+const cookieParser = require('cookie-parser');
+const logger =require('morgan');
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-export default app;
+app.use('/api/user', require('./routes/users'));
+
+module.exports=app;
