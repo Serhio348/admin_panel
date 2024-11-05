b8a98018213446d7d521047f0361a9dc3d1288e7
Siarhei Sidarovich <serhiosidorovich@gmail.com>
2024-11-05 15:15:09 +0300

    fealt:add function add employee

--- controllers/employees.js
+++ controllers/employees.js
@@ -14,8 +14,36 @@ const all = async (req, res) => {
       res.status(500).json({ message: "Не удалось получить сотрудников" });
     }
   };
-
+/**
+ * @route POST /api/employees/add
+ * @desc Добавление сотрудника
+ * @access Private
+ */
+const add = async (req, res) => {
+    try {
+      const data = req.body;
+  
+      if (!data.firstName || !data.lastName || !data.address || !data.age) {
+        return res.status(400).json({ message: "Все поля обязательные" });
+      }
+  
+      const employee = await prisma.employee.create({
+        data: {
+          ...data,
+          userId: req.user.id,
+        },
+      });
+  
+      return res.status(201).json(employee);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Что-то пошло не так" });
+    }
+  };
+  
+  
   module.exports = {
-    all
+    all,
+    add
     
   };
--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { all } = require("../controllers/employees");
+const { all, edit, remove, employee } = require("../controllers/employees");
 const { auth } = require('../middleware/auth');
 
 router.get("/", auth, all);
-
+router.get("/:id", auth, employee);
+router.post("/add", auth, add);
+router.post("/remove/:id", auth, remove);
+router.put("/edit/:id", auth, edit);
 
 module.exports = router;
