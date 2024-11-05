04794d659564744c679cbf5d247d79ed8b78a720
Siarhei Sidarovich <serhiosidorovich@gmail.com>
2024-11-05 16:01:45 +0300

    fealt:add remove and edit employee function

--- controllers/employees.js
+++ controllers/employees.js
@@ -40,10 +40,53 @@ const add = async (req, res) => {
       res.status(500).json({ message: "Что-то пошло не так" });
     }
   };
-  
+  /**
+ * @route POST /api/employees/remove/:id
+ * @desc Удаление сотрудника
+ * @access Private
+ */
+const remove = async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    await prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(204).json("OK");
+  } catch {
+    res.status(500).json({ message: "Не удалось удалить сотрудника" });
+  }
+};
+
+/**
+ * @route PUT /api/employees/edit/:id
+ * @desc Редактирование сотрудника
+ * @access Private
+ */
+const edit = async (req, res) => {
+  const data = req.body;
+  const id = data.id;
+
+  try {
+    await prisma.employee.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    res.status(204).json("OK");
+  } catch(err) {
+    res.status(500).json({ message: "Не удалось редактировать сотрудника" });
+  }
+};
   
   module.exports = {
     all,
-    add
-    
+    add,
+    remove,
+    edit
   };
--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { all, add } = require("../controllers/employees");
+const { all, add, remove, edit} = require("../controllers/employees");
 const { auth } = require('../middleware/auth');
 
 router.get("/", auth, all);
 router.post("/add", auth, add);
-
+router.post("/remove/:id", auth, remove);
+router.put("/edit/:id", auth, edit);
 module.exports = router;
